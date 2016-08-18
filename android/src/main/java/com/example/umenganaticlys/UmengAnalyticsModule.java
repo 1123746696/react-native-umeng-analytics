41b8086b5a97d58a0bd50a7783b090b6ae2670d6
jiangwolong2016 <wolong.jiang@b-and-qchian.com>
2016-08-18 11:27:29 +0800

    add android

---
+++ android/src/main/java/com/example/umenganaticlys/UmengAnalyticsModule.java
@@ -0,0 +1,54 @@
+package com.example.umenganaticlys;
+
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.umeng.analytics.MobclickAgent;
+
+/**
+ * Created by user on 16/6/15.
+ */
+public class UmengAnalyticsModule extends ReactContextBaseJavaModule {
+
+    public UmengAnalyticsModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+    @Override
+    public String getName() {
+        return "UmengAnalyticsModule";
+    }
+
+    /***
+     * 设置umeng的Key和渠道号
+     * @param key     umeng的key
+     * @param channelId  渠道号
+     */
+    @ReactMethod
+    public void setAppkeyAndChannelId(String key,String channelId) {
+        MobclickAgent.startWithConfigure(
+                new MobclickAgent.UMAnalyticsConfig(getCurrentActivity(), key, channelId, MobclickAgent.EScenarioType.E_UM_NORMAL));
+
+    }
+    @ReactMethod
+    public void beginLogPageView(String pageName) {
+        System.out.println("beginLogPageView＝＝＝＝"+pageName);
+        MobclickAgent.onPageStart(pageName);
+
+    }
+    @ReactMethod
+    public void endLogPageView(String pageName) {
+
+        System.out.println("endLogPageView＝＝＝＝"+pageName);
+        MobclickAgent.onPageEnd(pageName);
+
+    }
+    @ReactMethod
+    public void setEncryptEnabled(Boolean value) {
+        MobclickAgent.enableEncrypt(value);
+    }
+    @ReactMethod
+    public void event(String event) {
+        MobclickAgent.onEvent(getCurrentActivity(),event);
+    }
+}
